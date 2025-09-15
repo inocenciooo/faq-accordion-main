42495800149ab1ccceddb231e39a48af600fd6d2
vinicius inocêncio <vinicius1silva61@gmail.com>
2025-09-15 15:17:23 -0300

    js finalizado

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,9 @@
-const buttons = document.querySelectorAll(".button")
-const questions = document.querySelectorAll(".question")
+const buttons = document.querySelectorAll(".button");
+const questions = document.querySelectorAll(".question");
 
 buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-        button.parentElement.classList.toggle("active")
-        const image = button.querySelector("img")
-        
-        if (button.classList.contains("active")) {
-            image.src = "./assets/images/icon-plus.svg";
-        } else {
-            image.src = "./assets/images/icon-minus.svg";
-        }
-
-    })
-})
+  button.addEventListener("click", () => {
+    button.parentElement.classList.toggle("active");
+    const image = button.querySelector("img");
+  });
+});
